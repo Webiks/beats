8312491c245d85fb77741cca74daccf2fc49faff
Shachar Mossek <shachar.mossek@gmail.com>
2020-08-22 22:31:05 +0300

    Version 0.0.13 Added ManicTime Web/Desktop field and set applicationName accordingly Added ManicTime support for user switching Fixed ManicTime filter in query instead of after (performance boost) Update system.yml for manictime only

--- metricbeat/module/system/manictime/manictime.go
+++ metricbeat/module/system/manictime/manictime.go
@@ -32,13 +32,17 @@ func init() {
 // interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
-	database *sql.DB
-	userName string
+	database             *sql.DB
+	databasePathTemplate string
+	defaultUserName      string
+	lastUserName         string
+	webKeys              []string
 }
 
 type ManicTimeConfig struct {
-	Path string `yaml:"path"`
-	User string `yaml:"user"`
+	Path        string   `yaml:"path"`
+	DefaultUser string   `yaml:"defaultUser"`
+	WebKeys     []string `yaml:"webKeys"`
 }
 
 type Config struct {
@@ -68,6 +72,7 @@ type ValidActivity struct {
 	id              string
 	durationMin     float64
 	durationSec     float64
+	applicationType string
 	applicationName string
 }
 
@@ -86,19 +91,25 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	// read config
 	var cfg Config
-	readFile(&cfg)
+	readConfigFile(&cfg)
+
+	currentUserName := getCurrentUserName(cfg.Settings.DefaultUser)
+	databasePath := getUserDatabasePath(cfg.Settings.Path, currentUserName)
 
 	// connect to db
-	database, err := sql.Open("sqlite3", cfg.Settings.Path)
+	database, err := sql.Open("sqlite3", databasePath)
 	if err != nil {
-		fmt.Println("could not open db file of manicTime")
+		database = nil
+		fmt.Println("could not open db file of manicTime", databasePath)
 	}
 	// get current username
-	userName := getUserName(cfg)
 	return &MetricSet{
-		BaseMetricSet: base,
-		database:      database,
-		userName:      userName,
+		BaseMetricSet:        base,
+		databasePathTemplate: cfg.Settings.Path,
+		database:             database,
+		defaultUserName:      cfg.Settings.DefaultUser,
+		lastUserName:         currentUserName,
+		webKeys:              cfg.Settings.WebKeys,
 	}, nil
 }
 
@@ -106,13 +117,46 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch(report mb.ReporterV2) error {
+	currentUserName := getCurrentUserName(m.defaultUserName)
+	if currentUserName != m.lastUserName {
+		// Get last user name data
+		generateData(m, m.lastUserName, report)
 
+		// Set current user data
+		databasePath := getUserDatabasePath(m.databasePathTemplate, currentUserName)
+		database, err := sql.Open("sqlite3", databasePath)
+		if err != nil {
+			fmt.Println("Switching user but failed to load database", databasePath)
+			database = nil
+		}
+		m.database = database
+		m.lastUserName = currentUserName
+	} else if m.database == nil {
+		// Try to open database again
+		databasePath := getUserDatabasePath(m.databasePathTemplate, currentUserName)
+		database, err := sql.Open("sqlite3", databasePath)
+		if err == nil {
+			fmt.Print("Successful recovery of database", databasePath)
+			m.database = database
+		}
+	}
+
+	// Get current user name data
+	generateData(m, currentUserName, report)
+
+	return nil
+}
+
+func generateData(m *MetricSet, username string, report mb.ReporterV2) {
+	if m.database == nil {
+		return
+	}
 	lastSync := getLastSyncTime(m.database)
 	// parse string(date) to time
 	parsedLastSync, _ := time.Parse(time.RFC3339, lastSync)
 
 	// get all data
-	newData := getManicTimeNewData(m.database, parsedLastSync)
+	newData := getManicTimeNewData(m.database, m.webKeys, parsedLastSync)
 
 	for _, activity := range newData {
 		rootFields := common.MapStr{
@@ -126,6 +170,7 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) error {
 			"siteName":        activity.siteName,
 			"durationMin":     activity.durationMin,
 			"durationSec":     activity.durationSec,
+			"applicationType": activity.applicationType,
 			"applicationName": activity.applicationName,
 			"id":              activity.id,
 		}
@@ -135,20 +180,18 @@ func (m *MetricSet) Fetch(report mb.ReporterV2) error {
 			},
 			RootFields: common.MapStr{
 				"user": common.MapStr{
-					"name": m.userName,
+					"name": username,
 				},
 			},
 		})
 	}
 
-	//     update lastsync (memory + table)
+	// update lastsync (memory + table)
 	updateLastSync(m.database)
-
-	return nil
 }
 
 // read config file
-func readFile(cfg *Config) {
+func readConfigFile(cfg *Config) {
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
@@ -166,6 +209,13 @@ func readFile(cfg *Config) {
 		fmt.Println(err)
 	}
 
+}
+
+func getUserDatabasePath(pathTemplate string, username string) string {
+	userParts := strings.Split(username, "\\")
+	username = userParts[len(userParts)-1]
+	databasePath := strings.Replace(pathTemplate, "%USER%", username, 1)
+	return databasePath
 }
 
 func getLastSyncTime(database *sql.DB) string {
@@ -195,72 +245,86 @@ func getLastSyncTime(database *sql.DB) string {
 	return lastSync
 }
 
-func getManicTimeNewData(database *sql.DB, lastTimeSync time.Time) []ValidActivity {
+func isContains(slice []string, item string) bool {
+	for _, x := range slice {
+		if x == item {
+			return true
+		}
+	}
+	return false
+}
+
+var queryTemplate = `SELECT a.Name as title, a.StartUtcTime as startTime, a.EndUtcTime as endTime, b.Name as url, c.Key as appKey, c.Name as appName, d.Key as siteKey, d.Name as siteName
+FROM "Ar_Activity" as a
+LEFT JOIN "Ar_Activity" as b
+ON a.ActivityId = b.RelatedActivityId
+LEFT JOIN "Ar_CommonGroup" as c
+ON a.CommonGroupId = c.CommonId
+LEFT JOIN "Ar_CommonGroup" as d
+ON b.CommonGroupId = d.CommonId
+WHERE a.RelatedActivityId is NULL AND a.EndUtcTime >= '%v'`
+
+func getManicTimeNewData(database *sql.DB, webKeys []string, lastTimeSync time.Time) []ValidActivity {
 
 	var newData []ValidActivity
 
-	rows, _ := database.Query(`SELECT a.Name as title, a.StartUtcTime as startTime, a.EndUtcTime as endTime, b.Name as url, c.Key as appKey, c.Name as appName, d.Key as siteKey, d.Name as siteName
-	FROM "Ar_Activity" as a
-	LEFT JOIN "Ar_Activity" as b
-	ON a.ActivityId = b.RelatedActivityId
-	LEFT JOIN "Ar_CommonGroup" as c
-	ON a.CommonGroupId = c.CommonId
-	LEFT JOIN "Ar_CommonGroup" as d
-	ON b.CommonGroupId = d.CommonId
-	WHERE a.RelatedActivityId is NULL`)
+	rows, _ := database.Query(fmt.Sprintf(queryTemplate, lastTimeSync))
 
 	for rows.Next() {
 		p := Activity{}
 		err := rows.Scan(&p.title, &p.startTime, &p.endTime, &p.url, &p.appKey, &p.appName, &p.siteKey, &p.siteName)
 		if err != nil {
 			fmt.Println("error scanning rows in manictime metricset", err)
+			continue
 		}
-
 		if p.title == "Active" {
 			continue
 		}
-		parsedEndTime, _ := time.Parse(time.RFC3339, p.endTime)
-		if parsedEndTime.After(lastTimeSync) {
-			newActivity := ValidActivity{}
-			newActivity.title = p.title
-			newActivity.startTime = getMaxStartTime(p.startTime, lastTimeSync)
-			newActivity.endTime = p.endTime
-			if p.url.Valid {
-				newActivity.url = p.url.String
-			} else {
-				newActivity.url = ""
-			}
-			if p.appKey.Valid {
-				newActivity.appKey = p.appKey.String
-			} else {
-				newActivity.appKey = ""
-			}
-			if p.appName.Valid {
-				newActivity.appName = p.appName.String
-			} else {
-				newActivity.appName = ""
-			}
-			if p.siteKey.Valid {
-				newActivity.siteKey = p.siteKey.String
-			} else {
-				newActivity.siteKey = ""
-			}
-			if p.siteName.Valid {
-				newActivity.siteName = p.siteName.String
-				newActivity.applicationName = p.siteName.String
-			} else {
-				newActivity.siteName = ""
-				newActivity.applicationName = newActivity.appName
-			}
-			startTime, _ := time.Parse(time.RFC3339, newActivity.startTime)
-			endTime, _ := time.Parse(time.RFC3339, newActivity.endTime)
-			newActivity.durationMin = toFixed(endTime.Sub(startTime).Minutes(), 3)
-			newActivity.durationSec = endTime.Sub(startTime).Seconds()
-			hostname, _ := os.Hostname()
-			id := newActivity.appName + "_" + newActivity.startTime + "_" + hostname
-			newActivity.id = strings.ReplaceAll(id, " ", "_")
-			newData = append(newData, newActivity)
+		newActivity := ValidActivity{}
+		newActivity.title = p.title
+		newActivity.startTime = getMaxStartTime(p.startTime, lastTimeSync)
+		newActivity.endTime = p.endTime
+		if p.url.Valid {
+			newActivity.url = p.url.String
+		} else {
+			newActivity.url = ""
 		}
+		if p.appKey.Valid {
+			newActivity.appKey = p.appKey.String
+		} else {
+			newActivity.appKey = ""
+		}
+		if p.appName.Valid {
+			newActivity.appName = p.appName.String
+		} else {
+			newActivity.appName = ""
+		}
+		if p.siteKey.Valid {
+			newActivity.siteKey = p.siteKey.String
+		} else {
+			newActivity.siteKey = ""
+		}
+		if p.siteName.Valid {
+			newActivity.siteName = p.siteName.String
+		} else {
+			newActivity.siteName = ""
+		}
+		if p.appKey.Valid && isContains(webKeys, p.appKey.String) {
+			newActivity.applicationType = "Web"
+			newActivity.applicationName = p.siteName.String
+		} else {
+			newActivity.applicationType = "Desktop"
+			newActivity.applicationName = newActivity.appName
+		}
+		startTime, _ := time.Parse(time.RFC3339, newActivity.startTime)
+		endTime, _ := time.Parse(time.RFC3339, newActivity.endTime)
+		timeDiff := endTime.Sub(startTime)
+		newActivity.durationMin = toFixed(timeDiff.Minutes(), 3)
+		newActivity.durationSec = toFixed(timeDiff.Seconds(), 3)
+		hostname, _ := os.Hostname()
+		id := newActivity.appName + "_" + newActivity.startTime + "_" + hostname
+		newActivity.id = strings.ReplaceAll(id, " ", "_")
+		newData = append(newData, newActivity)
 	}
 	return newData
 }
@@ -279,7 +343,7 @@ func getMaxStartTime(startTime string, lastTimeSync time.Time) string {
 	return parsedStartTime.Format(time.RFC3339)
 }
 
-func getUserName(cfg Config) string {
+func getCurrentUserName(defaultUser string) string {
 	var username string
 	var query []Win32_ComputerSystem
 
@@ -290,7 +354,7 @@ func getUserName(cfg Config) string {
 	if query[0].UserName != "" {
 		username = query[0].UserName
 	} else {
-		username = cfg.Settings.User
+		username = defaultUser
 	}
 
 	return username
